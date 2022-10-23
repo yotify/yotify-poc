b55c9f5e7a85e24ed22d35b6ee1536c2e20117ed
Thomas Drebes <38798057+tdrebes@users.noreply.github.com>
2022-10-23 15:22:24 +0200

    Add project files.

---
+++ Yotify/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Yotify;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Yotify/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Yotify;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
